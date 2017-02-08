10d28fe669d02e9b1d27269e4d220de7ff773867
Victor <victor.em.017@gmail.com>
2017-02-07 23:26:26 -0800

    Creación de simulación de datos
    Creación de simulación de datos

--- WebApplication1/WebApplication1/Controllers/UsuarioController.cs
+++ WebApplication1/WebApplication1/Controllers/UsuarioController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -11,7 +12,22 @@ namespace WebApplication1.Controllers
         // GET: Usuario
         public ActionResult Index()
         {
-            return View();
+            List<UsuarioModel> usuarios = new List<UsuarioModel>();
+            UsuarioModel usuario = new UsuarioModel();
+            usuario.id = 1;
+            usuario.nombre = "Victor";
+            usuario.apellido = "E.M";
+
+            usuarios.Add(usuario);
+
+            UsuarioModel usuario2 = new UsuarioModel();
+            usuario2.id = 2;
+            usuario2.nombre = "Isra";
+            usuario2.apellido = "Sensei";
+
+            usuarios.Add(usuario2);
+
+            return View(usuarios);
         }
     }
 }
--- WebApplication1/WebApplication1/Models/UsuarioModel.cs
+++ WebApplication1/WebApplication1/Models/UsuarioModel.cs
@@ -7,5 +7,8 @@ namespace WebApplication1.Models
 {
     public class UsuarioModel
     {
+        public int id { get; set; }
+        public String nombre { get; set; }
+        public String apellido { get; set; }
     }
 }
